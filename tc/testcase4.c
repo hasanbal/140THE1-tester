d393ee38eedf085c399258a1f1a4b3012e09a4dc
Göktuğ Ekinci <58266670+GoktugEk@users.noreply.github.com>
2020-04-09 16:01:15 +0300

    Add files via upload

--- tc/testcase4.c
+++ tc/testcase4.c
@@ -18,7 +18,7 @@ int main() {
 	delete_node_rec(binary_tree,0);
 	draw_binary_tree_rec(binary_tree, 0, depth);
 	
-	insert_node(binary_tree,0,'r',5);
+	insert_node(binary_tree,0,'i',5);
 	draw_binary_tree_rec(binary_tree, 2, depth);
 	delete_node_rec(binary_tree,0);
 	
@@ -28,7 +28,7 @@ int main() {
 	printf("%d is at %d in tree with root[%d].\n", 3, breadth_first_search_itr(binary_tree, 1,3), 1);
 	printf("%d is at %d in tree with root[%d].\n", 2, breadth_first_search_itr(binary_tree, 0,2), 0);
 	printf("%d is at %d in tree with root[%d].\n", 4, breadth_first_search_itr(binary_tree, 0,4), 0);
-	printf("%d is at %d in tree with root[%d].\n", 5, breadth_first_search_itr(binary_tree, 0,5), 1);
+	printf("%d is at %d in tree with root[%d].\n", 5, breadth_first_search_itr(binary_tree, 0,5), 0);
 	
 	
 	
@@ -38,7 +38,7 @@ int main() {
 	printf("%d is at %d in tree with root[%d].\n", 3, depth_first_search_rec(binary_tree, 1,3), 1);
 	printf("%d is at %d in tree with root[%d].\n", 2, depth_first_search_rec(binary_tree, 0,2), 0);
 	printf("%d is at %d in tree with root[%d].\n", 4, depth_first_search_rec(binary_tree, 0,4), 0);
-	printf("%d is at %d in tree with root[%d].\n", 5, depth_first_search_rec(binary_tree, 0,5), 1);
+	printf("%d is at %d in tree with root[%d].\n", 5, depth_first_search_rec(binary_tree, 0,5), 0);
 	
 	printf("Find the height of the tree results:\n");
 	printf("The height of the tree is %d\n",find_height_of_tree_rec(binary_tree,0));
